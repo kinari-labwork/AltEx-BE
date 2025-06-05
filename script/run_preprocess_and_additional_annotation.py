c5fb7505d0ba39df78b7042d639b620c630a6cd7
kinari-labwork <matsumoto.kinari.qm@alumni.tsukuba.ac.jp>
2025-06-05 17:41:29 +0900

    バリアント情報のアノテーション実行コードを追加

--- script/run_preprocess_and_additional_annotation.py
+++ script/run_preprocess_and_additional_annotation.py
@@ -4,16 +4,14 @@ from preprocess_and_additional_annotation import (
     drop_abnormal_mapped_transcripts,
     cording_information_annotator,
     flame_information_annotator,
-    max_min_exon_count_annotator
+    max_min_exon_count_annotator,
+    variant_count_annotator
 )
-
-#export PYTHONPATH="$PYTHONPATH:./src" をbashで実行すること
+import ast
 
 data = pd.read_csv("data/exon_classification.csv")
-
-#transcript nameが重複している列を削除
-data = data.drop_duplicates(subset=["name"],keep=False)
-print(data.shape)
+#csvファイルのexonlengths列は文字列として読み込まれるため、リストに変換する
+data["exonlengths"] = data["exonlengths"].apply(ast.literal_eval)
 
 # 異常な染色体にマッピングされたトランスクリプトを削除
 data = drop_abnormal_mapped_transcripts(data)
@@ -24,5 +22,7 @@ data = cording_information_annotator(data)
 data = flame_information_annotator(data)
 # 最大および最小のエクソン数を追加
 data = max_min_exon_count_annotator(data)
+#遺伝子ごとのバリアントの数を追加
+data = variant_count_annotator(data)
 # データフレームを保存
 data.to_csv("data/exon_classification_with_additional_info.csv", index=False)
