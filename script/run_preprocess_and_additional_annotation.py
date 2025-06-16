4de038ef4f92481fc8c06dd6294bfb682494acdc
kinari-labwork <matsumoto.kinari.qm@alumni.tsukuba.ac.jp>
2025-06-16 10:06:15 +0900

    無効なimportを削除

--- script/run_preprocess_and_additional_annotation.py
+++ script/run_preprocess_and_additional_annotation.py
@@ -5,7 +5,6 @@ from preprocess_and_additional_annotation import (
     drop_abnormal_mapped_transcripts,
     cording_information_annotator,
     flame_information_annotator,
-    max_min_exon_count_annotator,
     variant_count_annotator
 )
 
@@ -22,4 +21,4 @@ data = flame_information_annotator(data)
 #遺伝子ごとのバリアントの数を追加
 data = variant_count_annotator(data)
 # データフレームを保存
-data.to_pickle("data/exon_classification_with_additional_info.pkl", index=False)
+data.to_pickle("data/exon_classification_with_additional_info.pkl")
