ab061d8d020276ad196591f1fadab9200980966b
kinari-labwork <matsumoto.kinari.qm@alumni.tsukuba.ac.jp>
2025-07-23 09:14:43 +0900

    update main.py

--- src/altex_aid/main.py
+++ src/altex_aid/main.py
@@ -7,6 +7,7 @@ from . import (
     sequence_annotator,
     splicing_event_classifier,
     target_exon_extractor,
+    sgrna_designer,
 )
 
 
@@ -120,6 +121,18 @@ def main():
         del splice_acceptor_single_exon_df, splice_donor_single_exon_df
 
         print("designing sgRNAs...")
+        target_exon_df_with_sgrna_info = sgrna_designer.design_sgrna_for_target_exon_df(
+            target_exon_df=target_exon_df_with_acceptor_and_donor_sequence,
+            pam_sequence="NGG",
+            editing_window_start_in_grna=17,
+            editing_window_end_in_grna=19,
+        )
+        target_exon_df_with_sgrna_info = sgrna_designer.organize_target_exon_df_with_grna_sequence(
+            target_exon_df_with_sgrna_info
+        )
+        target_exon_df_with_sgrna_info = sgrna_designer.convert_sgrna_start_end_position_to_position_in_chromosome(
+            target_exon_df_with_sgrna_info
+        )
 
 
 if __name__ == "__main__":
