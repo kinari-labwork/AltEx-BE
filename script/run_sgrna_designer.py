de68b64b169cd893d13f84e20f44b3f7c3297913
kinari-labwork <matsumoto.kinari.qm@alumni.tsukuba.ac.jp>
2025-07-30 17:23:46 +0900

    run_design_base_editorを修正済みdesign_base_editorに合うように修正

--- script/run_sgrna_designer.py
+++ script/run_sgrna_designer.py
@@ -1,22 +1,19 @@
 import pandas as pd
 from altex_aid.sgrna_designer import (
-    design_sgrna_for_target_exon_df,
-    organize_target_exon_df_with_grna_sequence,
-    convert_sgrna_start_end_position_to_position_in_chromosome,
+    design_sgrna_for_base_editors,
+    make_default_base_editors,
 )
 
 target_exons_df_with_acceptor_and_donor_sequence = pd.read_pickle("data/target_exons_with_acceptor_and_donor_sequence.pkl")
 
-
-# とりあえず target-AIDの条件で実行してみる
-target_exons_df_with_grna_info = design_sgrna_for_target_exon_df(
+base_editors = []
+base_editors = make_default_base_editors(base_editors)
+target_exons_df_with_sgrna = design_sgrna_for_base_editors(
     target_exon_df=target_exons_df_with_acceptor_and_donor_sequence,
-    pam_sequence="NGG",
-    editing_window_start_in_grna=17,
-    editing_window_end_in_grna=19
+    base_editor_list=base_editors
 )
-target_exons_df_with_grna_info = organize_target_exon_df_with_grna_sequence(target_exons_df_with_grna_info)
 
-target_exons_df_with_grna_info = convert_sgrna_start_end_position_to_position_in_chromosome(target_exons_df_with_grna_info)
 
-target_exons_df_with_grna_info.to_pickle("data/target_exons_with_grna_sequence.pkl")
+
+target_exons_df_with_sgrna.to_pickle("data/target_exons_with_sgrna.pkl")
+
