5abf7243eadb26ca74cd693898887490f7064130
kinari-labwork <matsumoto.kinari.qm@alumni.tsukuba.ac.jp>
2025-07-30 18:37:52 +0900

    , カンマ忘れを修正

--- script/run_sgrna_designer.py
+++ script/run_sgrna_designer.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from altex_aid.sgrna_designer import (
     design_sgrna_for_base_editors,
-    make_default_base_editors,
+    make_default_base_editors
 )
 
 target_exons_df_with_acceptor_and_donor_sequence = pd.read_pickle("data/target_exons_with_acceptor_and_donor_sequence.pkl")
