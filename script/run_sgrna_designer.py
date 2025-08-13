032214ea642e285df1b113ca70dca4ff0f778fb1
kinari-labwork <matsumoto.kinari.qm@alumni.tsukuba.ac.jp>
2025-08-13 10:02:33 +0900

    入力の引数が間違っていたので、正しいlistに修正した

--- script/run_sgrna_designer.py
+++ script/run_sgrna_designer.py
@@ -10,10 +10,10 @@ base_editors = make_preset_base_editors()
 base_editors_list = list(base_editors.values())
 target_exons_df_with_sgrna = design_sgrna_for_base_editors(
     target_exon_df=target_exons_df_with_acceptor_and_donor_sequence,
-    base_editors=base_editors
+    base_editors=base_editors_list
 )
 
-
+print(target_exons_df_with_sgrna.columns)
 
 target_exons_df_with_sgrna.to_pickle("data/target_exons_with_sgrna.pkl")
 
