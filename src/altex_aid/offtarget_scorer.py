160ce6a0709a053f94cf52bbfed5110a6a651b53
kinari-labwork <matsumoto.kinari.qm@alumni.tsukuba.ac.jp>
2025-08-16 12:45:03 +0900

    mappyを用いた簡易オフターゲット計算を更新。

--- src/altex_aid/offtarget_scorer.py
+++ src/altex_aid/offtarget_scorer.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import mappy as mp
-import pathlib as Path
+from pathlib import Path
+import io 
 
 def add_crisprdirect_url_to_df(exploded_sgrna_df: pd.DataFrame, assembly_name: str) -> pd.DataFrame:
     """
@@ -15,25 +16,36 @@ def add_crisprdirect_url_to_df(exploded_sgrna_df: pd.DataFrame, assembly_name: s
     exploded_sgrna_df["crisprdirect_url"] = base_url + target_sequences + "&pam=" + pams + "&db=" + assembly_name
     return exploded_sgrna_df
 
+def make_fasta_of_sgrnas(exploded_sgrna_df: pd.DataFrame) -> str:
+    """
+    Purpose: Create a FASTA format string from the exploded_sgrna_df.
+    """
+    fasta_str = ""
+    exploded_sgrna_df["sgrna_target_sequence"] = exploded_sgrna_df["sgrna_target_sequence"].str.replace('+', '', regex=False).str.lower()
+    for _, row in exploded_sgrna_df.iterrows():
+        fasta_str += f"> {row['uuid']}\n{row['sgrna_target_sequence']}\n"
+    return fasta_str
 
 def calculate_offtarget_site_count_to_df(exploded_sgrna_df: pd.DataFrame, fasta_path: Path) -> pd.DataFrame:
     """
     Purpose: exploded_sgrna_dfにオフターゲットの数を追加する
+    Parameters: exploded_sgrna_df: 入力のDataFrame, sgrna_fasta: sgRNAのFASTAファイル, fasta_path: 対象のFASTAファイルパス
     """
     aligner = mp.Aligner(str(fasta_path))
-    
-    # 計算対象のユニークな配列を取得
-    unique_sequences = exploded_sgrna_df["sgrna_target_sequence"].dropna().unique()
+    exploded_sgrna_fasta = make_fasta_of_sgrnas(exploded_sgrna_df)
 
-    # ユニークな配列に対してオフターゲット数を計算し、辞書に保存
-    offtarget_counts = {}
-    for seq in unique_sequences:
-        # mappyの正しい使い方。内部でインデックスが使われるため、FASTAを再読み込みする必要はない。
-        # また、"+"は塩基ではないため、シーケンスから除外すべき
-        clean_seq = seq.replace('+', '', 1).lower()
-        count = sum(1 for hit in aligner.map(clean_seq) if hit.mlen == len(clean_seq))
-        offtarget_counts[seq] = count
+    match_dict = {}
 
-    # 計算結果を元のDataFrameにマップする
-    exploded_sgrna_df["exact_match_count_of_20bp_plus_pam"] = exploded_sgrna_df["pam_plus_target_sequence"].map(offtarget_counts)
+    fasta_io = io.StringIO(exploded_sgrna_fasta)
+    for name, seq in mp.fastx_read(fasta_io): 
+        exact_match_count = 0
+        for hit in aligner.map(seq):
+            if hit.mlen == len(seq) and hit.NM == 0:
+                exact_match_count += 1
+            if exact_match_count > 10:
+                match_dict[name] = exact_match_count
+                break
+        match_dict[name] = exact_match_count
+
+    exploded_sgrna_df["pam+20bp_exact_match_count"] = exploded_sgrna_df["uuid"].map(match_dict).astype(int)
     return exploded_sgrna_df
