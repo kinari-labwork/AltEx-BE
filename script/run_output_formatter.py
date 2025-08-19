f8b43ea8851bf4a089c1c257b9e353bdeeb5638f
kinari-labwork <matsumoto.kinari.qm@alumni.tsukuba.ac.jp>
2025-08-19 15:12:04 +0900

    データのpathを正確なものに修正した

--- script/run_output_formatter.py
+++ script/run_output_formatter.py
@@ -4,11 +4,11 @@ import pickle
 from altex_aid.output_formatter import format_output
 from altex_aid.sgrna_designer import BaseEditor, make_preset_base_editors
 
-with open("target_exon_with_sgrna_dict.pkl", "rb") as f:
+with open("data/target_exons_with_sgrna_dict.pkl", "rb") as f:
     target_exons_df_with_sgrna_dict = pickle.load(f)
-
-exploded_classified_refflat = pd.read_csv("exploded_classified_refflat.csv")
 
 base_editors = make_preset_base_editors()
 
-formatted_exploded_sgrna_df = format_output(target_exons_df_with_sgrna_dict, exploded_classified_refflat, base_editors)
+formatted_exploded_sgrna_df = format_output(target_exons_df_with_sgrna_dict, base_editors)
+
+formatted_exploded_sgrna_df.to_pickle("data/formatted_exploded_sgrna_df.pkl")
