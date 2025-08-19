c8a549a99568d17e20e8f18f8efbc9fe619ede1a
kinari-labwork <matsumoto.kinari.qm@alumni.tsukuba.ac.jp>
2025-08-19 15:34:36 +0900

    pathを仮置きから正しいものに修正した

--- script/run_offtarget_scorer.py
+++ script/run_offtarget_scorer.py
@@ -5,12 +5,12 @@ from altex_aid.offtarget_scorer import (
     calculate_offtarget_site_count_simple
 )
 
-exploded_sgrna_df = pd.read_pickle("path/to/exploded_sgrna_df.pkl")
+exploded_sgrna_df = pd.read_pickle("data/formatted_exploded_sgrna_df.pkl")
 assembly_name = "mm39"
 
-fasta_path = Path("path/to/genome.fasta")
+fasta_path = Path("data/mm39/mm39.fa")
 
 exploded_sgrna_df_with_crispr_direct = add_crisprdirect_url_to_df(exploded_sgrna_df, assembly_name)
 exploded_sgrna_df_with_offtarget_counts = calculate_offtarget_site_count_simple(exploded_sgrna_df_with_crispr_direct, fasta_path)
 
-exploded_sgrna_df_with_offtarget_counts.to_pickle("./data/output_exploded_sgrna_df.pkl")
+exploded_sgrna_df_with_offtarget_counts.to_pickle("data/exploded_sgrna_df_with_offtarget_counts.pkl")
