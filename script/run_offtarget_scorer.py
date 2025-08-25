638f572c31a2f9c82a4251588b2fc1cdc45730a5
kinari-labwork <matsumoto.kinari.qm@alumni.tsukuba.ac.jp>
2025-08-25 17:57:55 +0900

    関数名を修正

--- script/run_offtarget_scorer.py
+++ script/run_offtarget_scorer.py
@@ -2,7 +2,7 @@ import pandas as pd
 from pathlib import Path
 from altex_aid.offtarget_scorer import (
     add_crisprdirect_url_to_df,
-    calculate_offtarget_site_count_simple
+    calculate_offtarget_site_count_ahocorasick
 )
 
 exploded_sgrna_df = pd.read_pickle("data/formatted_exploded_sgrna_df.pkl")
@@ -11,6 +11,6 @@ assembly_name = "mm39"
 fasta_path = Path("data/mm39/mm39.fa")
 
 exploded_sgrna_df_with_crispr_direct = add_crisprdirect_url_to_df(exploded_sgrna_df, assembly_name)
-exploded_sgrna_df_with_offtarget_counts = calculate_offtarget_site_count_simple(exploded_sgrna_df_with_crispr_direct, fasta_path)
+exploded_sgrna_df_with_offtarget_counts = calculate_offtarget_site_count_ahocorasick(exploded_sgrna_df_with_crispr_direct, fasta_path)
 
 exploded_sgrna_df_with_offtarget_counts.to_pickle("data/exploded_sgrna_df_with_offtarget_counts.pkl")
