ff74a1edf652a4deaa1985824621dea90cba942c
kinari-labwork <matsumoto.kinari.qm@alumni.tsukuba.ac.jp>
2025-08-29 14:10:49 +0900

    列名のミスを修正

--- src/altex_be/bed_for_ucsc_custom_track_maker.py
+++ src/altex_be/bed_for_ucsc_custom_track_maker.py
@@ -11,17 +11,17 @@ def format_sgrna_for_ucsc_custom_track(
     Return : pd.DataFrame 12 bedに修正された DataFrame
     """
     # score 列に100を超える値が入ることがあるため、100でクリップする
-    sgrna_df["pam+20bp_exact_match"] = sgrna_df["pam+20bp_exact_match"].apply(lambda x: 100 if x > 100 else x)
+    sgrna_df["pam+20bp_exact_match"] = sgrna_df["pam+20bp_exact_match_count"].apply(lambda x: 100 if x > 100 else x)
 
     bed_df = pd.DataFrame()
     bed_df["chrom"] = sgrna_df["chrom"]
     bed_df["chromStart"] = sgrna_df["sgrna_start_in_genome"]
     bed_df["chromEnd"] = sgrna_df["sgrna_end_in_genome"]
     bed_df["name"] = sgrna_df["geneName"] + "_" + sgrna_df["exontype"] + "_" + sgrna_df["base_editor_name"]  + "_" + sgrna_df["uuid"].str[:8]
-    bed_df["score"] = sgrna_df["pam+20bp_exact_match"]
+    bed_df["score"] = sgrna_df["pam+20bp_exact_match_count"]
     bed_df["strand"] = sgrna_df["strand"]
-    bed_df["thickStart"] = bed_df["sgrna_start_in_genome"]
-    bed_df["thickEnd"] = bed_df["sgrna_end_in_genome"]
+    bed_df["thickStart"] = bed_df["chromStart"] # 別に必要ないが、9bed にするために追加
+    bed_df["thickEnd"] = bed_df["chromEnd"]
 
     # Add RGB color based on base editor type
     color_map = {"abe": "255,0,0", "cbe": "0,0,255"} # ABE: red, CBE: blue
