5e58fa2c6088b1400adc9212c6997bca2d8e56fb
kinari-labwork <matsumoto.kinari.qm@alumni.tsukuba.ac.jp>
2025-10-30 15:10:41 +0900

    main 関数を切り分けて可読性とテスト可能性を上げた

--- src/altex_be/main.py
+++ src/altex_be/main.py
@@ -2,7 +2,6 @@ import argparse
 import pandas as pd
 from pathlib import Path
 import logging
-import sys
 import datetime
 from . import (
     refflat_preprocessor,
@@ -20,9 +19,10 @@ from .manage_arguments import (
     parse_arguments,
     validate_arguments
 )
+from .class_def.base_editors import BaseEditor
 
 
-def main():
+def run_pipeline():
     parser = build_parser.build_parser()
 
     args = parser.parse_args()
@@ -38,8 +38,6 @@ def main():
         assembly_name,
         parser
     )
-
-    output_track_name = f"{datetime.datetime.now().strftime('%Y%m%d%H%M')}_{assembly_name}_sgrnas_designed_by_altex-be"
     
     refflat = loding_and_preprocess_refflat(refflat_path, interest_gene_list, parser)
 
@@ -49,51 +47,37 @@ def main():
     splice_acceptor_single_exon_df, splice_donor_single_exon_df, exploded_classified_refflat = extract_target_exon(
         classified_refflat, interest_gene_list, parser
     )
-    
-    logging.info("-" * 50)
-    logging.info("Annotating sequences to dataframe from genome FASTA...")
-    logging.info(f"Using this FASTA file as reference genome: {fasta_path}")
-    target_exon_df_with_acceptor_and_donor_sequence = sequence_annotator.annotate_sequence_to_splice_sites(
-        exploded_classified_refflat, splice_acceptor_single_exon_df, splice_donor_single_exon_df, fasta_path
+    del classified_refflat
+
+    target_exon_df_with_acceptor_and_donor_sequence = annotate_sequences(
+        exploded_classified_refflat,
+        splice_acceptor_single_exon_df,
+        splice_donor_single_exon_df,
+        fasta_path
     )
     del splice_acceptor_single_exon_df, splice_donor_single_exon_df
-    
-    logging.info("designing sgRNAs...")
-    target_exon_df_with_sgrna_dict = sgrna_designer.design_sgrna_for_base_editors_dict(
-        target_exon_df=target_exon_df_with_acceptor_and_donor_sequence,
-        base_editors=base_editors
+
+    target_exon_df_with_sgrna_dict = design_sgrnas(
+        target_exon_df_with_acceptor_and_donor_sequence,
+        base_editors
     )
-    logging.info("-" * 50)
-    logging.info("Formatting output...")
-    formatted_exploded_sgrna_df = output_formatter.format_output(target_exon_df_with_sgrna_dict, base_editors)
-    if formatted_exploded_sgrna_df.empty:
-        logging.warning("No sgRNAs could be designed for given genes and Base Editors, Exiting")
-        sys.exit(0)
+    formatted_exploded_sgrna_df = format_output(target_exon_df_with_sgrna_dict, base_editors)
     del target_exon_df_with_acceptor_and_donor_sequence, exploded_classified_refflat
     
-    logging.info("Scoring off-targets...")
-    exploded_sgrna_with_offtarget_info = offtarget_scorer.score_offtargets(formatted_exploded_sgrna_df, assembly_name, fasta_path=fasta_path)
-    logging.info("-" * 50)
-    logging.info("Saving results...")
-    exploded_sgrna_with_offtarget_info.to_csv(output_directory / f"{output_track_name}_table.csv")
-    logging.info(f"Results saved to: {output_directory / f'{output_track_name}_table.csv'}")
+    exploded_sgrna_with_offtarget_info = score_offtargets(formatted_exploded_sgrna_df, assembly_name, fasta_path=fasta_path)
 
-    logging.info("Generating UCSC custom track...")
-    bed_df = bed_for_ucsc_custom_track_maker.format_sgrna_for_ucsc_custom_track(exploded_sgrna_with_offtarget_info)
-
-    output_path = output_directory / f"{output_track_name}_ucsc_custom_track.bed"
-    track_description: str = f"sgRNAs designed by AltEx-BE on {datetime.datetime.now().strftime('%Y%m%d')}"
-
-    with open(output_path, "w") as f:
-        track_header = f'track name="{output_track_name}" description="{track_description}" visibility=2 itemRgb="On"\n'
-        f.write(track_header)
-        bed_df.to_csv(f, sep="\t", header=False, index=False, lineterminator='\n')
-
-    logging.info(f"UCSC custom track file saved to: {output_path}")
+    output_track_name = f"{datetime.datetime.now().strftime('%Y%m%d%H%M')}_{assembly_name}_sgrnas_designed_by_altex-be"
+    save_results(exploded_sgrna_with_offtarget_info, output_directory, output_track_name)
+    write_ucsc_custom_track(
+        exploded_sgrna_with_offtarget_info,
+        output_directory,
+        output_track_name,
+        assembly_name
+    )
     
     logging.info("All AltEx-BE processes completed successfully.")
     logging.info("Printing summary of output:")
-    summary_dfs = validate_arguments.split_df_by_column_chunks(exploded_sgrna_with_offtarget_info, chunk_sizes=[12, 6, 6])
+    summary_dfs = split_df_by_column_chunks(exploded_sgrna_with_offtarget_info, chunk_sizes=[12, 6, 6])
     for sub_df in summary_dfs:
         print(sub_df)  # indexも表示される
         print("-" * 40)
@@ -135,6 +119,9 @@ def loding_and_preprocess_refflat(refflat_path: str, interest_gene_list: list[st
     return refflat
 
 def classify_splicing_events(refflat: pd.DataFrame) -> pd.DataFrame:
+    """
+    前処理されたrefflatデータフレームから、スプライシングイベントを分類する。
+    """
     logging.info("-" * 50)
     logging.info("Classifying splicing events...")
     classified_refflat = splicing_event_classifier.classify_splicing_events(refflat)
@@ -157,5 +144,103 @@ def extract_target_exon(classified_refflat: pd.DataFrame, interest_gene_list: li
             logging.info(f"Target exons found for the gene: {gene}.")
     return splice_acceptor_single_exon_df, splice_donor_single_exon_df, exploded_classified_refflat
 
+def annotate_sequences(
+    exploded_classified_refflat: pd.DataFrame,
+    splice_acceptor_single_exon_df: pd.DataFrame,
+    splice_donor_single_exon_df: pd.DataFrame,
+    fasta_path: str
+) -> pd.DataFrame:
+    logging.info("-" * 50)
+    logging.info("Annotating sequences to dataframe from genome FASTA...")
+    logging.info(f"Using this FASTA file as reference genome: {fasta_path}")
+    target_exon_df_with_acceptor_and_donor_sequence = sequence_annotator.annotate_sequence_to_splice_sites(
+        exploded_classified_refflat, splice_acceptor_single_exon_df, splice_donor_single_exon_df, fasta_path
+    )
+    return target_exon_df_with_acceptor_and_donor_sequence
+
+def design_sgrnas(
+    target_exon_df_with_acceptor_and_donor_sequence: pd.DataFrame,
+    base_editors: dict[str, BaseEditor]
+) -> dict[str, pd.DataFrame]:
+    logging.info("designing sgRNAs...")
+    target_exon_df_with_sgrna_dict = sgrna_designer.design_sgrna_for_base_editors_dict(
+        target_exon_df=target_exon_df_with_acceptor_and_donor_sequence,
+        base_editors=base_editors
+    )
+    return target_exon_df_with_sgrna_dict
+
+def format_output(
+    target_exon_df_with_sgrna_dict: dict[str, pd.DataFrame],
+    base_editors: dict[str, BaseEditor],
+    parser: argparse.ArgumentParser
+) -> pd.DataFrame:
+    logging.info("-" * 50)
+    logging.info("Formatting output...")
+    formatted_exploded_sgrna_df = output_formatter.format_output(target_exon_df_with_sgrna_dict, base_editors)
+    if formatted_exploded_sgrna_df.empty:
+        parser.error("No sgRNAs could be designed for given genes and Base Editors, Exiting")
+    return formatted_exploded_sgrna_df
+
+def score_offtargets(
+    formatted_exploded_sgrna_df: pd.DataFrame,
+    assembly_name: str,
+    fasta_path: str
+) -> pd.DataFrame:
+    logging.info("-" * 50)
+    logging.info("Scoring off-targets...")
+    exploded_sgrna_with_offtarget_info = offtarget_scorer.score_offtargets(formatted_exploded_sgrna_df, assembly_name, fasta_path=fasta_path)
+    logging.info("-" * 50)
+    return exploded_sgrna_with_offtarget_info
+
+def save_results(
+    exploded_sgrna_with_offtarget_info: pd.DataFrame,
+    output_directory: Path,
+    output_track_name: str
+) -> None:
+    logging.info("Saving results...")
+    exploded_sgrna_with_offtarget_info.to_csv(output_directory / f"{output_track_name}_table.csv")
+    logging.info(f"Results saved to: {output_directory / f'{output_track_name}_table.csv'}")
+    return
+
+def write_ucsc_custom_track(
+    exploded_sgrna_with_offtarget_info: pd.DataFrame,
+    output_directory: Path,
+    output_track_name: str,
+    assembly_name: str
+) -> None:
+    logging.info("Generating UCSC custom track...")
+    bed_df = bed_for_ucsc_custom_track_maker.format_sgrna_for_ucsc_custom_track(exploded_sgrna_with_offtarget_info)
+
+    output_path = output_directory / f"{output_track_name}_ucsc_custom_track.bed"
+    track_description: str = f"sgRNAs designed by AltEx-BE on {datetime.datetime.now().strftime('%Y%m%d')}"
+
+    with open(output_path, "w") as f:
+        track_header = f'track name="{output_track_name}" description="{track_description}" visibility=2 itemRgb="On"\n'
+        f.write(track_header)
+        bed_df.to_csv(f, sep="\t", header=False, index=False, lineterminator='\n')
+
+    logging.info(f"UCSC custom track file saved to: {output_path}")
+    return
+
+def split_df_by_column_chunks(df: pd.DataFrame, chunk_sizes=[12, 6, 6]) -> list[pd.DataFrame]:
+    """
+    DataFrameをchunk_sizesで指定したカラム数ごとに分割し、各DataFrameをリストで返す。
+    """
+    columns = df.columns.tolist()
+    idx = 0
+    df = df.head()
+    dfs = []
+    for size in chunk_sizes:
+        cols = columns[idx:idx+size]
+        if not cols:
+            break
+        dfs.append(df[cols].copy())
+        idx += size
+    # 残りのカラムも追加
+    if idx < len(columns):
+        cols = columns[idx:]
+        dfs.append(df[cols].copy())
+    return dfs
+
 if __name__ == "__main__":
-    main()
+    run_pipeline()
