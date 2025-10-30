6ebdac77449de13ed3bb6eb60579a77cc522994d
kinari-labwork <matsumoto.kinari.qm@alumni.tsukuba.ac.jp>
2025-10-30 14:07:18 +0900

    ディレクトリ移動によりパス変更

--- tests/test_refflat_preprocessor.py
+++ tests/test_refflat_preprocessor.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from altex_be.refflat_preprocessor import (
+from altex_be.loding_and_preprocess.refflat_preprocessor import (
     add_exon_position_flags,
     annotate_cording_information,
     annotate_flame_information,
