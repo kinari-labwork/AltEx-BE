dc8dacd6b18bf4e753a4c866bef461528cb1e074
kinari-labwork <matsumoto.kinari.qm@alumni.tsukuba.ac.jp>
2025-11-13 17:53:22 +0900

    テストを書き換えた

--- tests/test_gtf2refflat_converter.py
+++ tests/test_gtf2refflat_converter.py
@@ -3,7 +3,8 @@ from pathlib import Path
 from altex_be.gtf2refflat_converter import (
     run_gtf2genepred,
     convert_gtf_to_refflat_format,
-    add_genesymbol_to_imcomplete_refflat
+    add_genesymbol_to_imcomplete_refflat,
+    gtf_to_refflat,
 )
 
 def test_run_gtf2genepred():
@@ -30,4 +31,14 @@ def test_add_genesymbol_to_imcomplete_refflat():
     assert not refflat_with_genesymbol.empty
     assert refflat_with_genesymbol.shape[1] == 11  # geneName列が追加されていることを確認
     assert refflat_with_genesymbol.iloc[0,0].startswith("Gm")  # geneName列に正しい遺伝子記号が追加されていることを確認
+    return
+
+def test_gtf_to_refflat():
+    dummy_gtf_path = Path("tests/data/test.gtf")
+    output_path = Path("tests/data/output_refflat.txt")
+    gtf_to_refflat(dummy_gtf_path, output_path)
+    refflat_df = pd.read_csv(output_path, sep="\t", header=None)
+    assert not refflat_df.empty
+    assert refflat_df.shape[1] == 11  # geneName列が追加されていることを確認
+    assert refflat_df.iloc[0,0].startswith("Gm")  # geneName列に正しい遺伝子記号が追加されていることを確認
     return
